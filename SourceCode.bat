99c317615a6743268db79c3ad869e9163ba37c93
Universo-De-ProgramacaoGitHub <115424127+Universo-De-ProgramacaoGitHub@users.noreply.github.com>
2022-10-13 23:23:53 -0300

    Source Files Uploaded
    Upload feito

---
+++ SourceCode.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "C:\Users\%USERNAME%\Downloads\Englisher\bin"
+main.py
---
+++ setupExecutable.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIVJhxYRReKOXna
+::eg0/rx1wNQPfEVWB+kM9LVsJDBeHKH6qZg==
+::fBEirQZwNQPfEVWB+kM9LVsJDBeHKH6qI60Z6/3v7uiLpy0=
+::cRolqwZ3JBvQF1fEqQIDLQ9BXESGMyubFqV8
+::dhA7uBVwLU+EWGyK/VQ1OghbDACHfFuoCbIO6eX6TC0kYUJ9
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFC1dQg2BNX6pOpAS7+Ty/OKCsHEOUfYqaMHewrHu
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cd "C:\Users\%USERNAME%\Downloads\Englisher\bin"
+cd readers
+copy "C:\Users\%USERNAME%\Downloads\Englisher\bin\readers\Python" "C:\Users\%USERNAME%\AppData\Local\Programs"
